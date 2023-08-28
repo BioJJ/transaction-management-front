4905d7d8d85bda461d4f83e1756d257361590a1f
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-28 17:17:29 -0400

    feat: ✨ add constants

---
+++ src/constants/urls.ts
@@ -0,0 +1,15 @@
+export const URL_USER = 'http://localhost:3000/me'
+export const URL_USER_ID = 'http://localhost:3000/user/{userId}'
+export const URL_USER_ALL = 'http://localhost:3000/user/all'
+
+export const URL_AUTH = 'http://localhost:3000/login'
+export const URL_USER_CREATE = 'http://localhost:3000/users'
+
+export const URL_BILL = 'http://localhost:3000/energy-bill'
+export const URL_BILL_ID = 'http://localhost:3000/energy-bill/{id}'
+
+export const URL_CATEGORY = 'http://localhost:3000/category'
+export const URL_CATEGORY_ID = 'http://localhost:3000/category/{categoryId}'
+
+export const URL_ORDER_ID = 'http://localhost:3000/order/{orderId}'
+export const URL_ORDER_ALL = 'http://localhost:3000/order/all'
