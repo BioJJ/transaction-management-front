7b7c2efd08b701dd0a9d8111d00c644358a09e22
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-28 17:18:05 -0400

    feat: ✨ add functions connection

---
+++ src/functions/connections/auth.ts
@@ -0,0 +1,45 @@
+import { NavigateFunction, redirect } from 'react-router-dom'
+
+import { LoginRoutesEnum } from '../../routes/auth.routes'
+import { AUTHORIZATION_KEY } from '../../constants/authorizationConstants'
+import {
+	getItemStorage,
+	removeItemStorage,
+	setItemStorage
+} from './storageProxy'
+import { connectionAPIGet } from './connectionsAPI'
+import { URL_USER } from '../../constants/urls'
+import { UserType } from '../../types/UserType'
+
+export const unsetAuthorizationToken = () =>
+	removeItemStorage(AUTHORIZATION_KEY)
+
+export const setAuthorizationToken = (token?: string) => {
+	if (token) {
+		setItemStorage(AUTHORIZATION_KEY, token)
+	}
+}
+
+export const getAuthorizationToken = () => getItemStorage(AUTHORIZATION_KEY)
+
+export const verifyLoggedIn = async () => {
+	const token = getAuthorizationToken()
+
+	if (!token) {
+		return redirect(LoginRoutesEnum.LOGIN)
+	}
+	const user = await connectionAPIGet<UserType>(URL_USER).catch(() => {
+		unsetAuthorizationToken()
+	})
+
+	if (!user) {
+		return redirect(LoginRoutesEnum.LOGIN)
+	}
+
+	return null
+}
+
+export const logout = (navigate: NavigateFunction) => {
+	unsetAuthorizationToken()
+	navigate(LoginRoutesEnum.LOGIN)
+}
